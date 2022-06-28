c288917d737459b55fdb558aa4ec62c31eec44bb
Marco Alonso <marco@garagecoders.net>
2022-06-27 20:38:27 -0500

    Placeholder Added
    If the image doesn't load

--- AsyncImageApp/ContentView.swift
+++ AsyncImageApp/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let imageURL: String = "https://img.remediosdigitales.com/61a9ba/seat-ibiza-fr-20-aniversario-mexico-precio-/1366_2000.jpg"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // MARK: - 1.- Basic
+        
+        //AsyncImage(url: URL(string: imageURL))
+        
+        // MARK: - 2.- Scale
+        //AsyncImage(url: URL(string: imageURL), scale: 5.0)
+        
+        // MARK: - Placeholder
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 128)
+                .foregroundColor(.purple)
+                .opacity(0.5)
+        }
+        .padding(44)
     }
 }
 
