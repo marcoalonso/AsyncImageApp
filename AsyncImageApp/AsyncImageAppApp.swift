b80fab0983512f5c85624eb0e5e3f9b54528d961
Marco Alonso <marco@garagecoders.net>
2022-06-27 20:29:00 -0500

    Empezando

---
+++ AsyncImageApp/AsyncImageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageAppApp.swift
+//  AsyncImageApp
+//
+//  Created by marco rodriguez on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
